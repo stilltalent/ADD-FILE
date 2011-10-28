b348ccaa6c80c042408bec73d1907a1e62c67b8a
cloud <v2.cloud@gmail.com>
2011-10-28 11:22:47 +0800

    Initial Commit

---
+++ animations/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  animations
+//
+//  Created by  on 11-10-28.
+//  Copyright (c) 2011年 DremTop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ animations/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  animations
+//
+//  Created by  on 11-10-28.
+//  Copyright (c) 2011年 DremTop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
